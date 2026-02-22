b7e14853525ea5a9900d33191a9d8f02bd89d29b
Aseel <athamneh.aseel27@gmail.com>
2026-02-22 15:33:20 +0300

    Implement Discount entity and services

---
+++ ComputergyAPI/DTOs/Discount/DiscountInputDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace ComputergyAPI.DTOs.Discount
+{
+    public class DiscountInputDTO
+    {
+        public string Code { get; set; }
+
+        public string Percentage { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageURL { get; set; }
+
+        public int LimitUser { get; set; }
+
+        public float LimitAmount { get; set; }
+    }
+}
